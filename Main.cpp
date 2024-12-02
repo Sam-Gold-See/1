60b5ad6531004d426fe4198168b489d15734e6fd
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2024-12-02 22:46:50 +0800

    实现了Main函数启动

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,11 @@
 #include "include/MainWindow.h"
-#include "include/GameWindow.h"
+
 #include <QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    GameWindow y;
-    y.show();
+    MainWindow mainWindow;
+    mainWindow.show();
     return QApplication::exec();
 }
