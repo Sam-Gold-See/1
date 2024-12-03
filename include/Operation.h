7330c584f5a0ad8d6874ec16f748a920d54c103b
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2024-12-04 05:48:21 +0800

    手写Operation类、自定义operation结构体，使用栈数据结构实现了记录操作状态、复原操作状态、错误情况处理等功能

--- include/Operation.h
+++ include/Operation.h
@@ -9,19 +9,16 @@ struct operation
     int col;
     int value;
     bool isTrue;
-};
+};//使用结构体自定义operation记录、存储每次操作的数据
 
 class Operation
 {
 private:
-    std::stack<operation> operationStack;
+    std::stack<operation> operationStack;//关于操作记录，在底层使用栈结构来存储操作
 
 public:
-    Operation();
-    ~Operation();
-
-    void pushOperation(int row, int col, int value, bool isTrue);//
-    operation popOperation();//
+    void pushOperation(int row, int col, int value, bool isTrue);//进行一次操作前，将记录该操作落实前的该方块状态
+    operation popOperation();//pop出上次操作改变前的一个数独棋盘状态并自动回溯至深层次
 };
 
 #endif //OPERATION_H
--- src/Operation.cpp
+++ src/Operation.cpp
@@ -1,25 +1,17 @@
 #include "../include/Operation.h"
 
-Operation::Operation()
-{
-}
-
-Operation::~Operation()
-{
-}
-
 void Operation::pushOperation(int row, int col, int value, bool isTrue)
 {
-    operationStack.push({row, col, value, isTrue});
+    operationStack.push({row, col, value, isTrue}); //将传入数据封装成operation结构体并且压入栈中
 }
 
 operation Operation::popOperation()
 {
-    if (!operationStack.empty())
+    if (!operationStack.empty()) //检测是否栈空，以免造成栈越位
     {
-        operation temp = operationStack.top();
-        operationStack.pop();
-        return temp;
+        operation temp = operationStack.top();//获取当前栈内顶部的operation对象
+        operationStack.pop();//将当前栈顶部的operation对象弹出，便于在下次使用的时候返回更早之前的数独棋盘状态
+        return temp;//将获取到的operation对象返回到主程序中
     }
-    else return operation{-1, -1, -1, false};
+    else return operation{-1, -1, -1, false}; //如果栈空，则返回自定义的错误operation数据，让主程序根据此数据判断是否成功获取上次操作前的数独棋盘状态
 }
