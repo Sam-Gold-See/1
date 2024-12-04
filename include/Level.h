db87311728749fb0c8262e75ae8afa9e77c32b37
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2024-12-04 10:32:16 +0800

    手写Level类，实现了静态设置难度系数、数独挖空数、数独游戏难度显示文件源，使用静态方法保证全部属性、操作设置均为静态，以便做到唯一性（无实例化对象）便捷性（减少传递对象的损耗）

--- include/Level.h
+++ include/Level.h
@@ -3,13 +3,13 @@
 
 #include <iostream>
 
-class Level
+class Level//设置数独的难度类，因为有且仅有唯一Level，所以全部属性、操作均设置为静态，减少Level对象传递的耗时、误操作几率
 {
 public:
-    static int levelNum; //数独难度等级
-    static int digCount; //数独游戏挖空数
-    static std::string levelName; //数独游戏难度名
-    static void setLevel(int levelNumber); //设置数独游戏难度
+    static int levelNum;//数独游戏难度系数，用于设置Level类属性值
+    static int digCount; //数独游戏挖空数，用于生成数独，大致区分不同等级之间的难度
+    static std::string levelName; //数独游戏难度名，用于设置GameWindow的labelModeLevel图片源
+    static void setLevel(int levelNumber); //设置数独游戏难度，根据传入的难度系数设置属性
 };
 
 #endif //LEVEL_H
--- src/Level.cpp
+++ src/Level.cpp
@@ -1,30 +1,31 @@
 #include "../include/Level.h"
 #include "../include/Utils.h"
 
+//MainWindow窗口的初始静态UI需要Level对象的属性进行赋值，所以先初始化Level属性的值
 int Level::levelNum = 1;
 int Level::digCount = Utils::getRand(33, 2);
 std::string Level::levelName = "容易难度";
 
-void Level::setLevel(int levelNumber)
+void Level::setLevel(int levelNumber)//根据难度选择按钮返回的整数值，设置难度
 {
-    levelNum = levelNumber;
-    switch (levelNum)
+    levelNum = levelNumber;//记录传入整数值作为Level类属性值
+    switch (levelNum)//根据参数设置静态属性值
     {
     case 2:
-        digCount = Utils::getRand(37, 2);
-        levelName = "buttonModeNormal";
+        digCount = Utils::getRand(37, 2);//更新挖空数
+        levelName = "buttonModeNormal";//更新图片源名
         break;
     case 3:
-        digCount = Utils::getRand(42, 2);
-        levelName = "buttonModeHard";
+        digCount = Utils::getRand(42, 2);//更新挖空数
+        levelName = "buttonModeHard";//更新图片源名
         break;
     case 4:
-        digCount = Utils::getRand(47, 2);
-        levelName = "buttonModeHell";
+        digCount = Utils::getRand(47, 2);//更新挖空数
+        levelName = "buttonModeHell";//更新图片源名
         break;
     default:
-        digCount = Utils::getRand(32, 2);
-        levelName = "buttonModeEasy";
+        digCount = Utils::getRand(32, 2);//更新挖空数
+        levelName = "buttonModeEasy";//更新图片源名
         break;
     }
 }
