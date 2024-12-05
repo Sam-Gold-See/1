cf4ae6ffd200a482ce53966aef283e4a41c9b7e9
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2024-12-05 12:11:23 +0800

    实现MainWindow窗口的Logo显示与动态效果功能、爱心按钮的弹窗设置功能、游戏介绍的弹窗设置功能、难度选择与难度显示功能、开始游戏启动GameWindow窗口功能等

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -17,45 +17,45 @@ class MainWindow : public QWidget
     Q_OBJECT
 
 public:
-    MainWindow(QWidget* parent = nullptr);
-    ~MainWindow();
+    MainWindow(QWidget* parent = nullptr);//MainWindow窗口构造函数
+    ~MainWindow();//MainWindow的窗口析构函数
 
 private slots:
-    void on_buttonStart_clicked();
+    void on_buttonStart_clicked();//连接开始游戏按钮的信号与槽
 
-    void on_buttonLove_clicked() const;
+    void on_buttonLove_clicked() const;//连接爱心按钮的信号与槽
 
-    void on_buttonIntro_clicked() const;
+    void on_buttonIntro_clicked() const;//连接问号按钮的信号与槽
 
-    void on_buttonLoveText_clicked() const;
+    void on_buttonLoveText_clicked() const;//连接爱心按钮生成弹窗的信号与槽
 
-    void on_buttonIntroText_clicked() const;
+    void on_buttonIntroText_clicked() const;//连接问号按钮生成弹窗的信号与槽
 
-    void on_buttonBackground_clicked();
+    void on_buttonBackground_clicked();//连接背景窗口的信号与槽
 
-    void on_buttonModeHell_clicked();
+    void on_buttonModeHell_clicked();//连接地狱难度按钮的信号与槽
 
-    void on_buttonModeHard_clicked();
+    void on_buttonModeHard_clicked();//连接困难难度按钮的信号与槽
 
-    void on_buttonModeNormal_clicked();
+    void on_buttonModeNormal_clicked();//连接一般难度按钮的信号与槽
 
-    void on_buttonModeEasy_clicked();
+    void on_buttonModeEasy_clicked();//连接简单难度按钮的信号与槽
 
-    void setButtonModeVisible(bool flag) const;
+    void setButtonModeVisible(bool flag) const;//设置难度选择按钮的可视化程度
 
-    void moveButtonModeTogether() const;
+    void moveButtonModeTogether() const;//设置难度选择按钮移动至同一个位置
 
-    void moveButtonModeSeparately() const;
+    void moveButtonModeSeparately() const;//设置难度选择按钮分开到四个位置
 
-    void on_buttonLogo_clicked();
+    void on_buttonLogo_clicked();//设置左上角Logo按钮触发后的效果
 
-    void on_buttonLogoPressed_clicked();
+    void on_buttonLogoPressed_clicked();//设置中间大Logo生成后的操作效果
 
 private:
     Ui::MainWindow* ui;
-    bool selectFlag;
-    const int togetherX = 462;
-    const int togetherY = 491;
+    bool selectFlag;//难度选择系列按钮是否被点击过
+    const int togetherX = 462;//难度选择按钮的未点击状态X
+    const int togetherY = 491;//难度选择按钮的未点击状态Y
 
 };
 #endif // MAINWINDOW_H
--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -8,136 +8,135 @@ MainWindow::MainWindow(QWidget* parent)
     : QWidget(parent)
       , ui(new Ui::MainWindow)
 {
-    setWindowIcon(QIcon(":/resources/LOGO.ico"));
+    setWindowIcon(QIcon(":/resources/LOGO.ico")); //设置任务栏和窗口的Logo
 
-    ui->setupUi(this);
+    ui->setupUi(this); //显示MainWindow的所有静态设置UI
 
-    ui->buttonIntroText->setVisible(false);
-    ui->buttonLoveText->setVisible(false);
-    ui->buttonLogoPressed->setVisible(false);
+    ui->buttonIntroText->setVisible(false); //使得Intro弹窗先消失
+    ui->buttonLoveText->setVisible(false); //使得Love弹窗先消失
+    ui->buttonLogoPressed->setVisible(false); //使得Logo巨大化弹窗先消失
 
-    Utils::initUtils();
+    Utils::initUtils(); //初始化Utils类，即给rand()随机类提供随机种子
 
-    Level::setLevel(1);
-    selectFlag = false;
-    setButtonModeVisible(selectFlag);
-    moveButtonModeTogether();
-    ui->buttonModeEasy->setVisible(true);
+    selectFlag = false; //初始化难度选择按钮的点击状态，默认为未点击状态隐藏，即false
+    setButtonModeVisible(selectFlag); //设置四个难度按钮均隐藏
+    moveButtonModeTogether(); //设置四个难度选择按钮重叠
+    ui->buttonModeEasy->setVisible(true); //默认初始为简单难度，简单难度按钮可见
 }
 
-MainWindow::~MainWindow()
+MainWindow::~MainWindow() //MainWindow的析构函数
 {
-    delete ui;
+    delete ui; //删除MainWindow的静态UI界面
 }
 
-void MainWindow::on_buttonStart_clicked()
+void MainWindow::on_buttonStart_clicked() //连接了和开始按钮的信号与槽，设置该按钮触发的函数状态
 {
-    GameWindow* gameWindow = new GameWindow();
-    this->close();
-    gameWindow->show();
+    GameWindow* gameWindow = new GameWindow(); //设置GameWindow显现
+    this->close(); //关闭当前窗口，启用MainWindow的析构函数
+    gameWindow->show(); //设置刚生成的GameWindow的可见性为true
 }
 
-void MainWindow::on_buttonLove_clicked() const
+void MainWindow::on_buttonLove_clicked() const //连接了和爱心按钮的信号与槽，设置该按钮触发的函数状态
 {
-    ui->buttonLoveText->setVisible(true);
+    ui->buttonLoveText->setVisible(true); //设置了爱心文案弹窗的可见性为true
 }
 
-void MainWindow::on_buttonIntro_clicked() const
+void MainWindow::on_buttonIntro_clicked() const //连接了和问号按钮的信号与槽，设置该按钮触发的函数状态
 {
-    ui->buttonIntroText->setVisible(true);
+    ui->buttonIntroText->setVisible(true); //设置了问号文案弹窗的可见性为true
 }
 
-void MainWindow::on_buttonLoveText_clicked() const
+void MainWindow::on_buttonLoveText_clicked() const //连接了和爱心文案弹窗的信号与槽，设置该按钮触发的函数状态
 {
-    ui->buttonLoveText->setVisible(false);
+    ui->buttonLoveText->setVisible(false); //设置了爱心文案弹窗的可见性为false
 }
 
-void MainWindow::on_buttonIntroText_clicked() const
+void MainWindow::on_buttonIntroText_clicked() const //连接了和问号文案弹窗的信号与槽，设置该按钮触发的函数状态
 {
-    ui->buttonIntroText->setVisible(false);
+    ui->buttonIntroText->setVisible(false); //设置了问号文案弹窗的可见性为false
 }
 
-void MainWindow::on_buttonBackground_clicked()
+void MainWindow::on_buttonBackground_clicked() //连接了和背景窗口的信号与槽，设置该窗口点击触发的函数状态
 {
-    ui->buttonLoveText->setVisible(false);
-    ui->buttonIntroText->setVisible(false);
-    ui->buttonLogoPressed->setVisible(false);
-    ui->buttonLogo->setVisible(true);
+    ui->buttonLoveText->setVisible(false); //设置了爱心文案弹窗的可见性为false
+    ui->buttonIntroText->setVisible(false); //设置了问号文案弹窗的可见性为false
+    ui->buttonLogoPressed->setVisible(false); //设置了巨大化Logo弹窗的可见性为false
+    ui->buttonLogo->setVisible(true); //设置了左上角小logo弹窗的可见性为true
 }
 
-void MainWindow::on_buttonModeHell_clicked()
+void MainWindow::on_buttonModeHell_clicked() //设置地狱难度按钮的触发效果
 {
-    if (selectFlag)
+    if (selectFlag) //如果是选择难度状态
     {
-        Level::setLevel(4);
-        selectFlag = false;
-        setButtonModeVisible(selectFlag);
-        moveButtonModeTogether();
-        ui->buttonModeHell->setVisible(true);
+        Level::setLevel(4); //设置Level类的难度为4
+        selectFlag = false; //设置选择难度状态的布尔值为false，即非难度选择状态
+        setButtonModeVisible(selectFlag); //设置所有难度按钮的可见性为false
+        moveButtonModeTogether(); //设置四个难度按钮的位置统一
+        ui->buttonModeHell->setVisible(true); //设置地狱难度按钮的可见性为true
     }
-    else
+    else //如果非难度选择状态
     {
-        selectFlag = true;
-        moveButtonModeSeparately();
-        setButtonModeVisible(selectFlag);
-    }
-}
-
-void MainWindow::on_buttonModeHard_clicked()
-{
-    if (selectFlag)
-    {
-        Level::setLevel(3);
-        selectFlag = false;
-        setButtonModeVisible(selectFlag);
-        moveButtonModeTogether();
-        ui->buttonModeHard->setVisible(true);
-    }
-    else
-    {
-        selectFlag = true;
-        moveButtonModeSeparately();
-        setButtonModeVisible(selectFlag);
+        selectFlag = true; //设置选择难度状态的布尔值为true，即处于难度选择状态
+        moveButtonModeSeparately(); //设置四个难度选择按钮的坐标分离
+        setButtonModeVisible(selectFlag); //设置四个难度按钮的可见性为true
     }
 }
 
-void MainWindow::on_buttonModeNormal_clicked()
+void MainWindow::on_buttonModeHard_clicked() //设置困难难度按钮的触发效果
 {
-    if (selectFlag)
+    if (selectFlag) //如果是难度选择状态
     {
-        Level::setLevel(2);
-        selectFlag = false;
-        setButtonModeVisible(selectFlag);
-        moveButtonModeTogether();
-        ui->buttonModeNormal->setVisible(true);
+        Level::setLevel(3); //设置Level类的难度为3
+        selectFlag = false; //设置选择难度状态的布尔值为false，即非难度选择状态
+        setButtonModeVisible(selectFlag); //设置所有难度选择按钮的可见性为false
+        moveButtonModeTogether(); //设置四个难度按钮的位置统一
+        ui->buttonModeHard->setVisible(true);//设置困难难度按钮的可见性为true
     }
-    else
+    else//如果非难度选择状态
     {
-        selectFlag = true;
-        moveButtonModeSeparately();
-        setButtonModeVisible(selectFlag);
+        selectFlag = true;//设置选择难度状态的布尔值为true，即处于难度选择状态
+        moveButtonModeSeparately();//设置四个难度选择按钮的坐标分离
+        setButtonModeVisible(selectFlag);//设置四个难度按钮的可见性为true
     }
 }
 
-void MainWindow::on_buttonModeEasy_clicked()
+void MainWindow::on_buttonModeNormal_clicked()//设置一般难度按钮的触发效果
 {
-    if (selectFlag)
+    if (selectFlag)//如果是难度选择状态
     {
-        Level::setLevel(1);
-        selectFlag = false;
-        setButtonModeVisible(selectFlag);
-        moveButtonModeTogether();
-        ui->buttonModeEasy->setVisible(true);
+        Level::setLevel(2);//设置Level类的难度为2
+        selectFlag = false;//设置选择难度状态的布尔值为false，即非难度选择状态
+        setButtonModeVisible(selectFlag);//设置所有难度选择按钮的可见性为false
+        moveButtonModeTogether();//设置四个难度按钮的位置统一
+        ui->buttonModeNormal->setVisible(true);//设置一般难度按钮的可见性为true
     }
-    else
+    else//如果非难度选择状态
     {
-        selectFlag = true;
-        moveButtonModeSeparately();
-        setButtonModeVisible(selectFlag);
+        selectFlag = true;//设置选择难度状态的布尔值为true，即处于难度选择状态
+        moveButtonModeSeparately();//设置四个难度选择按钮的坐标分离
+        setButtonModeVisible(selectFlag);//设置四个难度按钮的可见性为true
     }
 }
 
-void MainWindow::setButtonModeVisible(bool selectFlag) const
+void MainWindow::on_buttonModeEasy_clicked()//设置简单难度按钮的触发效果
+{
+    if (selectFlag)//如果是难度选择状态
+    {
+        Level::setLevel(1);//设置Level类的难度为4
+        selectFlag = false;//设置选择难度状态的布尔值为false，即非难度选择状态
+        setButtonModeVisible(selectFlag);//设置所有难度选择按钮的可见性为false
+        moveButtonModeTogether();//设置四个难度按钮的位置统一
+        ui->buttonModeEasy->setVisible(true);//设置困难难度按钮的可见性为true
+    }
+    else//如果非难度选择状态
+    {
+        selectFlag = true;//设置选择难度状态的布尔值为true，即处于难度选择状态
+        moveButtonModeSeparately();//设置四个难度选择按钮的坐标分离
+        setButtonModeVisible(selectFlag);//设置四个难度按钮的可见性为true
+    }
+}
+
+void MainWindow::setButtonModeVisible(bool selectFlag) const//统一四个难度按钮的可见性设置
 {
     ui->buttonModeEasy->setVisible(selectFlag);
     ui->buttonModeNormal->setVisible(selectFlag);
@@ -145,7 +144,7 @@ void MainWindow::setButtonModeVisible(bool selectFlag) const
     ui->buttonModeHell->setVisible(selectFlag);
 }
 
-void MainWindow::moveButtonModeTogether() const
+void MainWindow::moveButtonModeTogether() const//统一四个难度按钮的坐标XY
 {
     ui->buttonModeEasy->move(togetherX, togetherY);
     ui->buttonModeNormal->move(togetherX, togetherY);
@@ -153,7 +152,7 @@ void MainWindow::moveButtonModeTogether() const
     ui->buttonModeHell->move(togetherX, togetherY);
 }
 
-void MainWindow::moveButtonModeSeparately() const
+void MainWindow::moveButtonModeSeparately() const//将四个难度选择按钮依次分开至四个位置
 {
     ui->buttonModeEasy->move(280, 465);
     ui->buttonModeNormal->move(645, 465);
@@ -161,15 +160,14 @@ void MainWindow::moveButtonModeSeparately() const
     ui->buttonModeHell->move(645, 570);
 }
 
-void MainWindow::on_buttonLogo_clicked()
+void MainWindow::on_buttonLogo_clicked()//连接了和logo按钮的信号与槽，设置该按钮触发的函数状态
 {
-    ui->buttonLogo->setVisible(false);
-    ui->buttonLogoPressed->setVisible(true);
+    ui->buttonLogo->setVisible(false);//设置左上角小logo的可见性为false
+    ui->buttonLogoPressed->setVisible(true);//设置logo巨大化版的可见性为true
 }
 
-void MainWindow::on_buttonLogoPressed_clicked()
+void MainWindow::on_buttonLogoPressed_clicked()//连接了和logo巨大化版的信号与槽，设置该按钮触发的函数状态
 {
-    ui->buttonLogo->setVisible(true);
-    ui->buttonLogoPressed->setVisible(false);
+    ui->buttonLogo->setVisible(true);//设置左上角小logo的可见性为true
+    ui->buttonLogoPressed->setVisible(false);//设置logo巨大化版的可见性为false
 }
-
